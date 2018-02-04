62c27035b7489a10e6ef5587d76ebf54d4372293
Mariela Atanasova <mariela_2001at@abv.bg>
2018-02-04 13:33:51 +0200

    masiviuprajneniq

---
+++ masiviuprajneniq/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masiviuprajneniq
+{
+    class Program
+    {
+
+        static string[] ReplaceMy(string command, string[] arr)
+        {
+            string[] result = arr;
+            string[] commandParam = command.Split(' ').ToArray();
+            string commandName = commandParam[0];
+            int index = int.Parse(commandParam[1]);
+            string word = commandParam[2];
+            result[index] = word;
+            return result;
+
+        }
+        static void Main(string[] args)
+        {
+
+            string[] numsList = Console.ReadLine().Split(' ').ToArray();
+
+            int n = int.Parse(Console.ReadLine()); //br comandi
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "Reverse":
+                        {
+                            Array.Reverse(numsList);
+                            break;
+                        }
+
+                    case "Distinct":
+                        {
+                            
+                            string[] newlist=numsList.Distinct().ToArray();
+                            Console.WriteLine(string.Join(" - ", newlist));
+                            break;
+
+                        }
+
+                    default:
+                        {
+                            numsList = ReplaceMy(command, numsList);
+                            break;
+                        }
+
+                        Console.WriteLine(string.Join(" - ", numsList));
+                }
+            }
+            //Console.WriteLine(string.Join(" - ", numsList));
+        }
+
+    }
+}
+
